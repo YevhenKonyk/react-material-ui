fac135effc4b5cd83140907dd521bf36a12918f2
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-07-30 20:43:20 +0300

    Added examples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  title: {
+    textTransform: 'uppercase',
+  },
+});
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <div>
-      <h1>Hello from App component</h1>
+      <Typography variant="h1" color="primary" className={classes.title}>
+        Hello from App component
+      </Typography>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
     </div>
   );
 };
